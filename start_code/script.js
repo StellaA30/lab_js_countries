e874c1bb5ba4c34988303302c316b99da026a6eb
Stella Annor <stella.annor@icloud.com>
2023-05-15 15:20:08 +0100

    adding solution to mvp tasks 1-3

--- start_code/script.js
+++ start_code/script.js
@@ -1,6 +1,43 @@
 console.log("Testing, testing, 1 2 3 ...")
 
+const pElement = document.querySelector("p");
+
 const fetchCountries = async () => {
     const countriesResponse = await fetch("https://restcountries.com/v3.1/all");
     const countriesJson = await countriesResponse.json();
-}  
+    console.log(countriesJson);
+    pElement.innerText = ""; // remove the p element ahead of populating the info
+    mapCountries(countriesJson); 
+}
+
+fetchCountries();
+
+
+const unOrderedList = document.querySelector("#countriesList");
+
+const mapCountries = (countriesList) => {
+
+    countriesList.forEach(country => {
+        const countryItem = document.createElement("li");
+        const countryName = country.name.common;
+        const countryCapital = country.capital;
+        const countryPopulation = country.population;
+
+        const countryFlag = document.createElement("img");
+        countryFlag.src = country.flags.png;
+        countryItem.innerHTML = "country name: " + countryName + " Capital: " + countryCapital+ " population: " + countryPopulation;
+        unOrderedList.appendChild(countryItem);
+        countryItem.appendChild(countryFlag);
+        
+    });
+
+}
+
+
+
+
+    
+
+
+
+
