5c95431c5690ae94badf64a00cb545119c5ac8c1
Stella Annor <stella.annor@icloud.com>
2023-05-15 13:46:34 +0100

    completed MVP task 1 to create function wuth fetch()

--- start_code/script.js
+++ start_code/script.js
@@ -1,1 +1,6 @@
 console.log("Testing, testing, 1 2 3 ...")
+
+const fetchCountries = async () => {
+    const countriesResponse = await fetch("https://restcountries.com/v3.1/all");
+    const countriesJson = await countriesResponse.json();
+}  
